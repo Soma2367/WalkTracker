4a49fd18e2fbc372f7804654d8004e900b4ac275
soma matsumoto <souma67jiji@icloud.com>
2026-03-14 16:38:58 +0900

    feat: Userルート追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function admin()
+    {
+        return $this->belongsTo(Admin::class);
+    }
 }
